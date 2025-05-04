7ce79d2053352381672422b66c26cbbde5081622
Caio Nascimento Battista <caio.caiobattista@outlook.com>
2025-05-04 03:13:05 -0300

    feat: adição do header/navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <nav className="sticky top-0 bg-white w-full shadow z-50">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="#home" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img
+                        src="src\assets\images\Furia_Esports_logo.png"
+                        className="w-15 h-auto"
+                        alt="Logo FURIA"
+                    />
+                </a>
+
+                <div className="flex md:order-2 space-x-3 md:space-x-1 rtl:space-x-reverse">
+                    <button
+                        type="button"
+                        className="text-white bg-blue-950 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center cursor-pointer"
+                    >
+                        Começar agora
+                    </button>
+
+                    <button
+                        onClick={toggleMenu}
+                        type="button"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                        aria-controls="navbar-sticky"
+                        aria-expanded={isMenuOpen}
+                    >
+                        <span className="sr-only">Abrir menu principal</span>
+                        <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 17 14"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M1 1h15M1 7h15M1 13h15"
+                            />
+                        </svg>
+                    </button>
+                </div>
+
+                <div
+                    className={`${isMenuOpen ? 'block' : 'hidden'
+                        } items-center justify-between w-full md:flex md:w-auto md:order-1`}
+                    id="navbar-sticky"
+                >
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+                        <li>
+                            <a
+                                href="#home"
+                                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                            >
+                                Início
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#about"
+                                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                            >
+                                Sobre
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#services"
+                                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                            >
+                                Serviços
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#team"
+                                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                            >
+                                Nossa equipe
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
